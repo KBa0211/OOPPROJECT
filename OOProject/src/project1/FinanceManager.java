ca39dcc52cd511c0107b999eb72f75b2e9a555e8
KBa0211 <62286221+KBa0211@users.noreply.github.com>
2024-12-22 02:02:09 +0600

    Add files via upload

---
+++ OOProject/src/project1/FinanceManager.java
@@ -0,0 +1,28 @@
+package project1;
+
+public class FinanceManager extends Employee {
+    private double budget;
+
+    public FinanceManager(String name, int id, double budget) {
+        super(name, id);
+        this.budget = budget;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    public void approveTransaction(double amount) {
+        if (amount > budget) {
+            System.out.println("Transaction denied: Insufficient budget.");
+        } else {
+            budget -= amount;
+            System.out.println("Transaction approved. Remaining budget: " + budget);
+        }
+    }
+}
+
